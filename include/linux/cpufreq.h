435ba2982bafbd1aa00001f204489121d8c1bf51
Alfredo <alfredomonclus@gmail.com>
2013-07-01 15:06:48 -0300

    cpufreq: add LulzactiveW governor
    Change-Id: If2e4ba858a3eaea80b98f02d903a313980f8375c

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -365,6 +365,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEW)
+extern struct cpufreq_governor cpufreq_gov_lulzactivew
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lulzactivew)
 #endif
 
 
